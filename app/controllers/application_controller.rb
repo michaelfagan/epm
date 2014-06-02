dd98a820a916ad5a37401b853ad2d997bb70d10b
Michael Fagan <mfagan@gmail.com>
2014-06-02 15:22:25 -0400

    custom 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  unless Rails.application.config.consider_all_requests_local
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  end
+
   check_authorization unless: :devise_controller?
   rescue_from CanCan::AccessDenied do |exception|
    if current_user
@@ -17,6 +21,10 @@ class ApplicationController < ActionController::Base
     def after_sign_in_path_for(resource)
       return edit_user_path(resource) unless resource.sign_in_count > 2 || resource.has_full_profile?
       super
+    end
+
+    def record_not_found
+      render 'shared/404', status: 404
     end
 
 end
